61e26539bc482f7864bb063c68c4dadf65954a16
Keats <keatsaudrey10@gmail.com>
2026-08-29 21:37:02 +0800

    feature: User Model, AppDbContext & EF Core Setup

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string PasswordHash { get; set; } = string.Empty;
+}
